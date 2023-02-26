44c31fed8462021b59c70e795c975f8c452f8cd1
Atul upadhyay <atulupadhyay82@gmail.com>
2023-02-25 20:51:14 -0500

    Adding implicit assertion

--- cypress/e2e/Mytest.cy.js
+++ cypress/e2e/Mytest.cy.js
@@ -19,5 +19,28 @@ describe('suite name',() => {
         cy.get("#login-button").click();
         cy.xpath("//div[@class='inventory_item']").should("have.length",6);   
         
-     })
+     }),
+     it('implicit assertion using Should,and',  function(){
+      cy.visit("https://www.saucedemo.com");
+      cy.get("#user-name").type("standard_user");
+      cy.get("#password").type("secret_sauce");
+      cy.get("#login-button").click();
+      cy.get("span.title").contains("Products");  
+      
+      cy.url().should('include','saucedemo');
+      cy.url().should('eq','https://www.saucedemo.com');
+      cy.url().should('contain','saucedemo');
+
+      //Another way to write is, you don't need to write cy.url() and capturing the url multiple times
+      cy.url().should('include','saucedemo')
+      .should('eq','https://www.saucedemo.com') 
+      .should('contain','saucedemo');
+
+      //Another way is to use and instead of using Should multiple times
+      cy.url().should('include','saucedemo')
+      .and('eq','https://www.saucedemo.com') 
+      .and('contain','saucedemo');
+
+
+   })
 })
